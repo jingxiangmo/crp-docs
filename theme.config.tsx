cd025e2aa0ca1508d9651de127db9dce31f516f1
Jingxiang Mo <jingxiangmo@gmail.com>
2024-05-15 03:15:28 +0900

    Update

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Made with ❤️ by JXM and Sasha',
+    text: 'Made with ❤️ by JXM and ',
   },
 }
 
