11a007b06728a4425ecb4785d89c9c8fcccd7b07
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-08 00:27:02 -0600

    links to author's websites and prettier on that file

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+import Link from "next/link";
 
 const config: DocsThemeConfig = {
   logo: <span>Create ROS Project</span>,
   project: {
-    link: 'https://github.com/jingxiangmo/create-ros-project',
+    link: "https://github.com/jingxiangmo/create-ros-project",
   },
   chat: {
-    link: 'https://discord.gg/bu6yn3ncB7',
+    link: "https://discord.gg/bu6yn3ncB7",
   },
-  docsRepositoryBase: 'https://github.com/jingxiangmo/create-ros-project',
+  docsRepositoryBase: "https://github.com/jingxiangmo/create-ros-project",
   footer: {
-    text: 'Made with ❤️ by JXM and Beau',
+    text: (
+      <span>
+        Made with ❤️ by{" "}
+        <Link className="author-link" href="https://jingxiangmo.com">
+          JXM
+        </Link>{" "}
+        and{" "}
+        <Link className="author-link" href="https://beaumccartney.vercel.app">
+          Beau
+        </Link>
+      </span>
+    ),
   },
-}
+};
 
-export default config
+export default config;
